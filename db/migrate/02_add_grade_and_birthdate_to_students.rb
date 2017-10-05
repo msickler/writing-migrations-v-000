555cb3d9de9dcc330a32c5595a1561b778b772d1
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-05 17:03:15 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,7 @@
 class More_students < ActiveRecord::Migration
 
   def change
-    add_column :students, :grade, :integer 
+    add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
   end
 
